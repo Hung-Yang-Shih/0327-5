769183ec7d6179a918ec2c89971de022ea4f778d
Hung-Yang-Shih <d11416204@gs.takming.edu.tw>
2026-03-27 12:10:20 +0800

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+public class Student
+{
+    private int ID;
+    private string Name;
+    private Test Score;
+    public static int Count;
+
+    public Student(int id, string n, int m, int f)
+    {
+        ID=id;
+        Name=n;
+        Score = new Test(m, f);
+        Count++;
+    }
+
+    public string GetStudent()
+    {
+        string str = "";
+        str = "學生編號-" + ID.ToString();
+        str += "\n學生姓名-" + Name;
+        str += "\n學生期中考試-" + Score.GetMid();
+        str += "\n學生期末考試-" + Score.GetFinal();
+        return str;
+    }
+
+    public static int StdCount()
+    {
+        return Count;
+    }
+
+}
